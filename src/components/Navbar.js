04bd8e37c499ac09700efebc64cba9b0d6ee48e1
broncs <jhowfrombrazil@gmail.com>
2021-01-06 08:18:57 -0300

    change color on Scroll from top

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FaLaptopCode } from "react-icons/fa";
 import { FaBars } from "react-icons/fa";
 import Modal from "./Modal";
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const container = useRef(null);
 
   // const handleClick = () => {
   //   setToggle((prevValue) => !prevValue);
   // };
+
+  const handleScroll = (e) => {
+    if (window.scrollY === 0) {
+      setIsScrolled(false);
+    } else {
+      setIsScrolled(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const modalWillOpen = () => {
     setToggle(true);
@@ -28,13 +45,16 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className="navbar">
+    <nav
+      className={isScrolled ? "navbar  nav-background" : "navbar"}
+      ref={container}
+    >
       <div className="container">
-        <div className="nav-container">
+        <div className={`nav-container`}>
           <div className="nav-header">
             <FaLaptopCode className="nav-logo" />
 
-            <button className="btn toggle-btn" onClick={modalWillOpen}>
+            <button className="toggle-btn" onClick={modalWillOpen}>
               <FaBars />
             </button>
           </div>
