7a4a791b65dc26815aa0f9d8681b2b9c9327bfbf
broncs <jhowfrombrazil@gmail.com>
2021-01-08 11:12:47 -0300

    same

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -31,11 +31,12 @@ const Contact = () => {
       <div className="container">
         <div className="form-contact">
           <form
+            name="contact"
             method="POST"
             className="contact-form"
             onSubmit={handleSubmit(onSubmit)}
           >
-            {/* <input type="hidden" name="form-name" value="contact" /> */}
+            <input type="hidden" name="form-name" value="contact" />
 
             <h2>Deixe uma Mensagem!</h2>
             <input
